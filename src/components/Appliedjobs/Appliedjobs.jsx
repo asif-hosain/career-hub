31bd7db289e8b19d30992a91d8913160debeda24
asif-hosain <asifhdoha@gmail.com>
2023-09-24 00:14:49 +0600

    header Footer Added

---
+++ src/components/Appliedjobs/Appliedjobs.jsx
@@ -0,0 +1,10 @@
+
+const Appliedjobs = () => {
+    return (
+        <div>
+            <h2>Jobs I applied</h2>
+        </div>
+    );
+};
+
+export default Appliedjobs;
---
+++ src/main.jsx
@@ -0,0 +1,37 @@
+import React from 'react'
+import ReactDOM from 'react-dom/client'
+import {
+  createBrowserRouter,
+  RouterProvider,
+} from "react-router-dom";
+import './index.css'
+import Root from './components/Root/Root';
+import Home from './components/Home/Home';
+import Appliedjobs from './components/Appliedjobs/Appliedjobs';
+import ErrorPage from './components/ErrorPage/ErrorPage';
+
+
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <Root></Root>,
+    errorElement:<ErrorPage></ErrorPage>,
+    children: [
+      {
+        path: '/',
+        element: <Home></Home>
+      },
+      {
+        path: '/applied',
+        element: <Appliedjobs></Appliedjobs>
+      }
+    ]
+  },
+]);
+
+ReactDOM.createRoot(document.getElementById('root')).render(
+  <React.StrictMode>
+    {/* <App /> */}
+    <RouterProvider router={router} />
+  </React.StrictMode>,
+)
