8249e2422b83c907da71c2943b9e2bd1eafa1d51
asif-hosain <asifhdoha@gmail.com>
2023-09-24 10:01:07 +0600

    Data Added

---
+++ src/components/CategoryList/CategoryList.jsx
@@ -0,0 +1,10 @@
+const CategoryList = () => {
+    return (
+        <div>
+            <h2 className="text-6xl text-center">Job Category list</h2>
+            <p></p>
+        </div>
+    );
+};
+
+export default CategoryList;
